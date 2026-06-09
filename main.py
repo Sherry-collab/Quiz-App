bb664b80c093eae71ac3c962fec04c0ab36a899c
sherry <sherry@Grims-MacBook-Air.local>
2026-06-09 22:34:20 +0500

    Apply .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+import models, schemas, database
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-@app.get('/blog')
-def get():
-    return {"details": "Hey hey hey"}
+get_db = database.get_db
+
+@app.post("/login")
+def login(request: schemas.Login, db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(
+        models.User.email == request.email
+    ).first()
+
+    if not user:
+        raise HTTPException(status_code=404,
+                            detail="User not found")
+
+    if user.password != request.password: 
+        raise HTTPException(status_code=401,
+                            detail="Invalid password")
+
+    return {
+        "message": "Login successful",
+        "role": user.role
+    }
 
